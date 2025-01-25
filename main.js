06bbbc2d319ae32901825995bb5c29e156f5a24f
David Palacios <awaken90@gmail.com>
2025-01-24 21:24:52 -0300

    Repo para entregas de Java Script.

---
+++ main.js
@@ -0,0 +1,48 @@
+// Menú de comida como array
+const menu = [
+  { nombre: "Pizza", precio: 5000 },
+  { nombre: "Hamburguesa", precio: 5500 },
+  { nombre: "Papas", precio: 3500 },
+  { nombre: "Ensalada", precio: 2000 },
+  { nombre: "Cerveza", precio: 2000 },
+];
+
+// Función para mostrar el menú
+function mostrarMenu() {
+  let menuTexto = "Menú disponible:\n";
+  menu.forEach((item, index) => {
+    menuTexto += `${index + 1}. ${item.nombre} - $${item.precio}\n`;
+  });
+  return menuTexto;
+}
+
+// Simulador del menú
+function simuladorMenu() {
+  alert("¡Bienvenido al simulador de pedidos.");
+
+  let seguir = true;
+    while (seguir) {
+    const menuTexto = mostrarMenu();
+    const eleccion = prompt(
+      menuTexto + "\nElegí un número del menú (1 a " + (menu.length) + "):"
+    );
+
+    const indice = parseInt(eleccion) - 1;
+    if (indice >= 0 && indice < menu.length) {
+      const producto = menu[indice];
+      alert(`Elegiste: ${producto.nombre}.\nPrecio: $${producto.precio}.`);
+    } else if(indice === menu.length) {
+        alert("Gracias por usar mi simulador!");
+        seguir = false;
+    } else {
+        alert("Opción no válida. Por favor volvé a intentar."); //sólo funciona si se ingresan letras,
+                                                                //ingresar un número mayor a 5 cierra el ciclo.
+                                                                //AVERIGUAR
+    }
+  }
+
+  console.log("Gracias por usar mi simulador!");
+}
+
+// Iniciar el simulador
+simuladorMenu();
